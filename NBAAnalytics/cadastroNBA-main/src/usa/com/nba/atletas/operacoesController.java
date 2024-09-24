e83e82f8669263e5266e18c109dd8414ef13e4e9
Espedito227456 <144754549+Espedito227456@users.noreply.github.com>
2024-09-23 21:01:00 -0300

    Add files via upload

---
+++ NBAAnalytics/cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
@@ -0,0 +1,26 @@
+package usa.com.nba.atletas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class operacoesController {
+	
+	@FXML
+	private TextField txfNome;
+	
+	@FXML
+	private TextField txfSobrenome;
+	
+	@FXML
+	private TextField txfIdade;
+	
+	@FXML
+	private TextField txfPosicao;
+	
+	@FXML
+	private TextField txfEquipe;
+	
+	@FXML
+	private Button btn_Cadastrar;
+}
